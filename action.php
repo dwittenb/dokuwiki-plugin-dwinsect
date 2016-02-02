a864c4231269d77d186fe66df303c15bfa1e786b
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:07:08 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -33,7 +33,7 @@ class action_plugin_dwinsect extends DokuWiki_Action_Plugin {
    * Register the eventhandlers
    * @see DokuWiki_Action_Plugin::register()
    */
-  function register(&$controller) {
+  function register(Doku_Event_Handler $controller) {
   	$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array()); 
   }
   
--- syntax.php
+++ syntax.php
@@ -116,7 +116,7 @@ class syntax_plugin_dwinsect extends DokuWiki_Syntax_Plugin {
    /**
    * Handle the match
    */
-  function handle($match, $state, $pos, &$handler){
+  function handle($match, $state, $pos, Doku_Handler $handler){
   	
 	  switch ($state) {
 			case DOKU_LEXER_ENTER:
@@ -206,7 +206,7 @@ class syntax_plugin_dwinsect extends DokuWiki_Syntax_Plugin {
   /**
    * Create output
    */
-	function render($mode, &$renderer, $data) {
+	function render($mode, Doku_Renderer $renderer, $data) {
 
 		if($mode == 'xhtml'){
 			list($state, $result) = $data;
